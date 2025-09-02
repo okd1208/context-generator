88c23304292e2015db05f696d1cead72a1df0edc
okadyyy <68635282+okd1208@users.noreply.github.com>
2025-09-02 18:58:31 +0900

    chore: append semicolon to analytics import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
